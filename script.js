4f417fae1d018955ea0e20a1d05d6bcf95816488
Vinicius Silva <vsaguiar@outlook.com>
2024-06-05 05:09:27 -0300

    testando github pages

---
+++ script.js
@@ -0,0 +1,15 @@
+document.getElementById('btnWhatsApp').addEventListener('click', function() {
+    window.open('https://api.whatsapp.com/message/F2WQIX3GAQWPD1?autoload=1&app_absent=0', '_blank');
+});
+
+document.getElementById('instagram-menu').addEventListener('click', function() {
+    window.open('https://www.instagram.com/recantodovale.eventos', '_blank');
+});
+
+document.getElementById('instagram-footer').addEventListener('click', function() {
+    window.open('https://www.instagram.com/recantodovale.eventos', '_blank');
+});
+
+document.getElementById('whatsapp-menu').addEventListener('click', function() {
+    window.open('https://api.whatsapp.com/message/F2WQIX3GAQWPD1?autoload=1&app_absent=0', '_blank');
+});
